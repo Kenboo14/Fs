dd4a3167a81f3d470ee3d2920f65db0883bd5400
Kenboox <116920242+Kenboo14@users.noreply.github.com>
2023-04-25 17:45:18 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -43,8 +43,8 @@ def start_button(client):
                 InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
-                InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink4),
+                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ3", url=client.invitelink3),
+                InlineKeyboardButton(text="ɢʀᴏᴜᴘ4", url=client.invitelink4),
             ],
             [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close")],
         ]
@@ -95,8 +95,8 @@ def fsub_button(client, message):
                 InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
-                InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink4),
+                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ3", url=client.invitelink3),
+                InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ4", url=client.invitelink4),
             ],
 
         ]
@@ -127,7 +127,7 @@ def start_button(client):
     if not FORCE_SUB_CHANNELS and FORCE_SUB_GROUPS:
         buttons = [
             [
-                InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink4),
+                InlineKeyboardButton(text="ɢʀᴏᴜᴘ4", url=client.invitelink4),
             ],
             [
                 InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
@@ -138,7 +138,7 @@ def start_button(client):
     if FORCE_SUB_CHANNELS and not FORCE_SUB_GROUPS:
         buttons = [
             [
-                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
+                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ3", url=client.invitelink3),
             ],
             [
                 InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
@@ -156,8 +156,8 @@ def start_button(client):
                 InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
-                InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink4),
+                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ3", url=client.invitelink3),
+                InlineKeyboardButton(text="ɢʀᴏᴜᴘ4", url=client.invitelink4),
             ],
             [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close")],
         ]
